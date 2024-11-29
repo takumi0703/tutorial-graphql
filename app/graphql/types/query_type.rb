94bb0189ac63325cccf9ab4e655711acd100c78b
takumi otani <takumiotani@takuminonotobukkukonpyuta.local>
2024-11-29 11:22:26 +0900

    配列クエリ作成　

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,34 +1,38 @@
 # frozen_string_literal: true
 
 # GraphQLのクエリ内容を定義するクラス
+# fieldを追加した場合は、再起動(docker-compose up)が必要
 module Types
   class QueryType < Types::BaseObject
     field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
       argument :id, ID, required: true, description: "ID of the object."
     end
-
     def node(id:)
       context.schema.object_from_id(id, context)
     end
-
     field :nodes, [Types::NodeType, null: true], null: true, description: "Fetches a list of objects given a list of IDs." do
       argument :ids, [ID], required: true, description: "IDs of the objects."
     end
-
     def nodes(ids:)
       ids.map { |id| context.schema.object_from_id(id, context) }
     end
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    # 例
+    # クエリで指定する時は、testField(キャメルケース)である必要がある
+    # field :test_field, String, null: false,
+    #   description: "An example field added by the generator"
+    # def test_field
+    #   "Hello World!"
+    # end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
-
+    # クエリ例
+    # {
+    #   user(id: "1") {
+    #     id
+    #     name
+    #     email
+    #   }
+    # }
     field :user, Types::UserType, null: false do
       argument :id, ID, required: true
     end
@@ -36,6 +40,22 @@ module Types
     # メソッド名とfieldの名前が一致している必要があるよう
     def user(id:)
       User.find(id)
+    end
+
+    # 複数の場合は、第二引数にtypeを[]で囲う
+    # クエリ例
+    # {
+    #   users {
+    #     id
+    #     name
+    #     email
+    #   }
+    # }
+    field :users, [Types::UserType], null: false
+    # field に指定された値に対して返す値を定義する
+    # メソッド名とfieldの名前が一致している必要があるよう
+    def users
+      User.all
     end
   end
 end
